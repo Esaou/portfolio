6b6af707b63242d00a10faa78637001c3e6a65f4
Esaou <eric.saou3@gmail.com>
2021-09-27 10:57:32 +0200

    codacy issues correction js

--- public/js/Validator.js
+++ public/js/Validator.js
@@ -1,34 +1,35 @@
 class Validator {
 
-    constructor() {
+    constructor()
+    {
 
-        this.firstname = document.getElementById('firstname');
-        this.lastname = document.getElementById('lastname');
-        this.email = document.getElementById('email');
-        this.password = document.getElementById('password');
-        this.confirmPassword = document.getElementById('passwordConfirm');
-        this.content = document.getElementById('content');
-        this.submitRegister = document.getElementById('submitRegister');
-        this.submitContact = document.getElementById('submitContact');
-        this.submitAccount = document.getElementById('submitAccount');
-        this.error = document.getElementById('error');
+        this.firstname = document.getElementById("firstname");
+        this.lastname = document.getElementById("lastname");
+        this.email = document.getElementById("email");
+        this.password = document.getElementById("password");
+        this.confirmPassword = document.getElementById("passwordConfirm");
+        this.content = document.getElementById("content");
+        this.submitRegister = document.getElementById("submitRegister");
+        this.submitContact = document.getElementById("submitContact");
+        this.submitAccount = document.getElementById("submitAccount");
+        this.error = document.getElementById("error");
 
         this.passCheck = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,100})$/;
 
-        if(this.submitRegister){
-            this.submitRegister.addEventListener('click', (event)=>{
+        if (this.submitRegister) {
+            this.submitRegister.addEventListener("click", (event)=>{
                 this.registerValidator(event);
             });
         }
 
-        if(this.submitContact){
-            this.submitContact.addEventListener('click', (event)=>{
+        if (this.submitContact) {
+            this.submitContact.addEventListener("click", (event)=>{
                 this.contactValidator(event);
             });
         }
 
-        if(this.submitAccount){
-            this.submitAccount.addEventListener('click', (event)=>{
+        if (this.submitAccount) {
+            this.submitAccount.addEventListener("click", (event)=>{
                 this.registerValidator(event);
             });
         }
@@ -37,22 +38,22 @@ class Validator {
 
     // Les différentes méthodes pour le fonctionnement de la validation
 
-    registerValidator(event){
+    registerValidator(event)
+    {
 
-        if (this.firstname.value === '' || this.lastname.value === '' || this.email.value === '' || this.password.value === '' ||this.confirmPassword.value === ''){
+        if (this.firstname.value === '' || this.lastname.value === '' || this.email.value === '' || this.password.value === '' ||this.confirmPassword.value === '') {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Tous les champs doivent être remplis !';
-        } else if (this.firstname.value.length < 2 || this.firstname.value.length > 30 || this.lastname.value.length < 2 || this.lastname.value.length > 30){
+        } else if (this.firstname.value.length < 2 || this.firstname.value.length > 30 || this.lastname.value.length < 2 || this.lastname.value.length > 30) {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Le prénom et le nom doivent contenir de 2 à 30 caractères !';
-        } else if (this.confirmPassword.value !== this.password.value){
+        } else if (this.confirmPassword.value !== this.password.value) {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Mots de passe non identiques !';
-
-        } else if (!this.passCheck.test(this.confirmPassword.value) && !this.passCheck.test(this.confirmPassword.value)){
+        } else if (!this.passCheck.test(this.confirmPassword.value) && !this.passCheck.test(this.confirmPassword.value)) {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Votre mot de passe doit contenir au moins 1 chiffre, une lettre minuscule, majuscule, un caractère spécial et 8 caractères minimum&nbsp;!';
@@ -60,13 +61,14 @@ class Validator {
 
     }
 
-    contactValidator(event){
+    contactValidator(event)
+    {
 
-        if (this.firstname.value === '' || this.lastname.value === '' || this.email.value === '' || this.content.value === ''){
+        if (this.firstname.value === '' || this.lastname.value === '' || this.email.value === '' || this.content.value === '') {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Tous les champs doivent être remplis !';
-        } else if (this.firstname.value.length < 2 || this.firstname.value.length > 30 || this.lastname.value.length < 2 || this.lastname.value.length > 30){
+        } else if (this.firstname.value.length < 2 || this.firstname.value.length > 30 || this.lastname.value.length < 2 || this.lastname.value.length > 30) {
             event.preventDefault();
             this.error.style.display = 'flex';
             this.error.innerHTML = 'Le prénom et le nom doivent contenir de 2 à 30 caractères !';
